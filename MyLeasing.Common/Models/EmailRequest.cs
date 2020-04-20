823e9514b4eaeba4de66d76089700b75f111930e
frajavi3 <frajavi3@hotmail.com>
2020-04-20 09:16:17 -0700

    Owners Api Controller

---
+++ MyLeasing.Common/Models/EmailRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MyLeasing.Common.Models
+{
+    public class EmailRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
